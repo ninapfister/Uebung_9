2bd956320002ceb4cc77502194366cf35a56ad28
ninapfister <1810653169@stud.fh-kufstein.ac.at>
2019-01-26 19:42:08 +0100

    Aufgabe1nichtfertig

---
+++ src/Student.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Student
+{
+    String vorname;
+    String nachname;
+    int alter;
+    int matrikelnummer;
+    String nationalitat;
+
+public static void main (String[]args)
+{
+    ArrayList<Student>
+}
